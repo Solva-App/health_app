ee0370904336c1162ffadf8c349c2f3880f1b99e
Fhave <omosiyobo@gmail.com>
2026-04-23 15:57:04 +0100

    adjust db config

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize')
 const logger = require('../utils/logger')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.BD_HOST,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   logging: false,
   pool: {
